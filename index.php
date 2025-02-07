d7f52caedf152b0a1ac9d08de1d7223c2c13df3f
paytakhtdota <paytakhtdota@gmail.com>
2025-02-07 02:50:15 -0800

    navbar styles

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
-<?php
-phpinfo();
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="styles\main.css">
+    <link rel="stylesheet" href="styles\nav.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cisw 410 final</title>
 </head>
 <body>
-    
+    <div id="main-container">
+<?php
+include("navbar.php");
+?>
+</div>
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="left-side">
+        <a href="#"><img src="images/logo2.png" alt="logo"></a>
+        <a href="#">Home</a>
+        <a href="#">Reservation</a>
+        <a href="#">Contact</a>
+        <a href="#">About</a>
+    </div>
+    <div class="right-side">
+<button class="button-61">Sign Up</button>
+<button class="button-61">Log In</button>
+    </div>
+</nav>
