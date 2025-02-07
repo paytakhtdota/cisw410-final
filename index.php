16f6b471e0aabc03fee93f1f780c1b5acd50b278
paytakhtdota <paytakhtdota@gmail.com>
2025-02-07 11:11:25 -0800

    slider added

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="styles\main.css">
     <link rel="stylesheet" href="styles\nav.css">
+    <link rel="stylesheet" href="styles\slider.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cisw 410 final</title>
 </head>
+
 <body>
-    <div id="main-container">
-<?php
-include("navbar.php");
-?>
-</div>
+    <?php
+    include("navbar.php");
+    ?>
+
+            <?php
+            include("slider.php");
+            ?>
+
+            <div style="width:100%; background-color:black; height:100vh;">
+hello
+        </div>
+        <div style="width:100%; background-color:black; height:100vh;">
+hello
+        </div>
 </body>
+
 </html>
