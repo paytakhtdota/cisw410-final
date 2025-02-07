17f51afc18f73e5c2700fad1639fb9dfdeefd89e
paytakhtdota <paytakhtdota@gmail.com>
2025-02-06 22:19:47 -0800

    test commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ phpinfo();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>cisw 410 final</title>
 </head>
 <body>
     
