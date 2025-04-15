9f5354b63f6313fbe8c5691a9bb8bdec2726854a
Mohammad Ansari <paytakhtdota@gmail.com>
2025-04-15 09:44:06 -0700

    commit

--- ticketprocess.php
+++ ticketprocess.php
@@ -9,8 +9,34 @@ if (!isset($_SESSION['user_data'])) {
     echo "OK!";
 }
 
+
+function InsertTicket($guestName,$idUser,$idEvent,$idSeat){
+    require_once("connection.php");
+    try {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $upQuery = $pdo->prepare("INSERT INTO address(guest_name, id_seat, id_event, id_user) VALUES (:guest_name,:id_seat,:id_event,:id_user)");
+        if (
+            $upQuery->execute([
+                ":guest_name" => $guestName,
+                ":id_seat" => $idSeat,
+                ":id_event" => $idEvent,
+                ":id_user" => $idUser
+            ])
+        ) {
+            echo "<script>Ticket added Successfuly!</script>";
+        } else {
+            echo "<script>Ticket insert failed!</script>";
+        }
+
+    } catch (PDOException $e) {
+        $msg = "fail: " . $e->getMessage();
+        redirectToErrorPage($msg);
+    }
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   
+
     if (isset($_POST['ticket']) && is_array($_POST['ticket'])) {
         foreach ($_POST['ticket'] as $seatId => $seatData) {
 
@@ -19,12 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $idEvent = isset($seatData['id_event']) ? $seatData['id_event'] : '';
             $idSeat = isset($seatData['id_seat']) ? $seatData['id_seat'] : '';
 
-            echo "</br>";
-            echo "Array index: $seatId<br>";
-            echo "Guest Name: $guestName<br>";
-            echo "User ID: $idUser<br>";
-            echo "Event ID: $idEvent<br>";
-            echo "Seat ID: $idSeat<br>";
+            InsertTicket($guestName,$idUser,$idEvent,$idSeat);            
         }
     } else {
         redirectToErrorPage("Sorry! It seems something wrong; Plese try again later!");
